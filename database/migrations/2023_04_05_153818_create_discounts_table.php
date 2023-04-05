51e82e7e485d7483293a82f817b628dec698984e
Maman` <75215687+JfeStudio@users.noreply.github.com>
2023-04-05 22:40:54 +0700

    add feature discount camp | schema db

---
+++ database/migrations/2023_04_05_153818_create_discounts_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create("discounts", function (Blueprint $table) {
+            $table->id();
+            $table->string("name", 100);
+            $table->string("code", 5);
+            $table->integer("percentage");
+            $table->text("description")->nullable();
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists("discounts");
+    }
+};
